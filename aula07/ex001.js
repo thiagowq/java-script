59accecbeed87ab13bf9ac4f8f3ce2a8821bcb12
Thiago José da Cruz <tj3137488@gmail.com>
2025-08-22 12:39:17 -0300

    .

---
+++ aula07/ex001.js
@@ -0,0 +1,7 @@
+
+var n1 = Number.parseInt(window.prompt('Digite um número: ')) //String
+var n2 = Number.parseInt(window.prompt('Digite outro número: ')) //String
+var soma = n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`) // Concatenação
+// number + number para adição
+// string + string para concatenação
